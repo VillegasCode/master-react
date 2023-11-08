2ffcc977f6e907f191c49e5468fc9b26ea891a47
Carlos Villegas <carlos.villegasch@gmail.com>
2023-11-08 14:05:35 -0500

    Assets(resources) imported like styles, responsive, normalize css

--- 17-proyecto4/src/main.jsx
+++ 17-proyecto4/src/main.jsx
@@ -1,10 +1,17 @@
+//Importaciones obligatorias de REACT
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
 
+//Importar assets (recursos, hojas de estilos, imágenes)
+import './assets/fonts/fontawesome-free-6.1.2-web/css/all.css';
+import './assets/css/normalize.css';
+import './assets/css/styles.css';
+import './assets/css/responsive.css';
+
+//Arrancar app de react
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  
     <App />
-  </React.StrictMode>,
+  
 )
