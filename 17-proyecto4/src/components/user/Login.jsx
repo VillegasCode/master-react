a3fc96477e7eec771e75aada55d08bd9a279f534
Carlos Villegas <carlos.villegasch@gmail.com>
2023-11-09 14:09:53 -0500

    Get data from user form through the changed metod and saved on form state

--- 17-proyecto4/src/components/user/Login.jsx
+++ 17-proyecto4/src/components/user/Login.jsx
@@ -1,6 +1,26 @@
 import React from 'react'
+import { useForm } from '../../hooks/useForm';
 
 export const Login = () => {
+
+  //Rellenar el estado de form con el método changed que se importan del componente HOOKS useForm.jsx
+  const { form, changed } = useForm({});
+
+  const loginUser = async (e) => {
+    e.preventDefault();
+
+    //Recoger los datos del usuario a través del método changes y guardarlos en el estado form
+    console.log(form);
+    let userToLogin = form;
+
+    //Peticion al backend
+
+
+    //Persistir los datos en el navegador
+
+
+  }
+
   return (
     <>
       <header className="content__header content__header--public">
@@ -8,16 +28,17 @@ export const Login = () => {
       </header>
 
       <div className="content__posts">
-        <form className='form-login'>
+
+        <form className='form-login' onSubmit={loginUser}>
 
           <div className='form-group'>
             <label htmlFor='email'>Email</label>
-            <input type='email' name='email' />
+            <input type='email' name='email' onChange={changed} />
           </div>
 
           <div className='form-group'>
             <label htmlFor='password'>Contraseña</label>
-            <input type='password' name='password' />
+            <input type='password' name='password' onChange={changed} />
           </div>
 
           <input type='submit' value="Identifícate" className="btn btn-success" />
