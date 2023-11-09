bf15b678fc85057951bea7d6d09aa9cbc5a68d59
Carlos Villegas <carlos.villegasch@gmail.com>
2023-11-08 20:27:14 -0500

    useForm imported and form, changed hook created to save a new user

--- 17-proyecto4/src/components/user/Register.jsx
+++ 17-proyecto4/src/components/user/Register.jsx
@@ -1,6 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { useForm } from '../../hooks/useForm';
 
 export const Register = () => {
+
+  const {form, changed} = useForm({});
+
+  const saveUser = async(e) => {
+    //Prevenir actualización de pantalla al presionar el botón registrar
+    e.preventDefault();
+
+    //Recoger datos del formulario
+    let newUser = form;
+
+    //Guardar usuario en el backend
+    console.log(newUser);
+    //const request = await fetch("url completa", opciones)
+  }
+
+
   return (
     <>
       <header className="content__header content__header--public">
@@ -8,7 +25,29 @@ export const Register = () => {
       </header>
 
       <div className="content__posts">
+        <form className='register-form' onSubmit={saveUser}>
+          <div className='form-group'>
+            <label htmlFor='name'>Nombre</label>
+            <input type='text' name='name' onChange={changed} />
+          </div>
 
+          <div className='form-group'>
+            <label htmlFor='surname'>Apellidos</label>
+            <input type='text' name='surname' onChange={changed} />
+          </div>
+
+          <div className='form-group'>
+            <label htmlFor='email'>Correo Electrónico</label>
+            <input type='email' name='email' onChange={changed} />
+          </div>
+
+          <div className='form-group'>
+            <label htmlFor='password'>Contraseña</label>
+            <input type='password' name='password' onChange={changed} />
+          </div>
+
+          <input type='submit' value='Regístrate' className='btn btn-success' onChange={changed} />
+        </form>
       </div>
     </>
   )
