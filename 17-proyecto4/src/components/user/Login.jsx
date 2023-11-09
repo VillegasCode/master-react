e259081928495ffa2e3b0f82782a4c9b7ceb20fc
Carlos Villegas <carlos.villegasch@gmail.com>
2023-11-09 13:44:02 -0500

    Creating structure HTML for login a form

--- 17-proyecto4/src/components/user/Login.jsx
+++ 17-proyecto4/src/components/user/Login.jsx
@@ -8,7 +8,21 @@ export const Login = () => {
       </header>
 
       <div className="content__posts">
+        <form className='form-login'>
 
+          <div className='form-group'>
+            <label htmlFor='email'>Email</label>
+            <input type='email' name='email' />
+          </div>
+
+          <div className='form-group'>
+            <label htmlFor='password'>Contraseña</label>
+            <input type='password' name='password' />
+          </div>
+
+          <input type='submit' value="Identifícate" className="btn btn-success" />
+
+        </form>
       </div>
     </>
   )
