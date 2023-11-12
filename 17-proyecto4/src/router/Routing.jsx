e8b6f0cea9e997bff9fcbfd8b837108097b8ec63
Carlos Villegas <carlos.villegasch@gmail.com>
2023-11-12 12:55:26 -0500

    Setting routes within private page like logout, people, settings

--- 17-proyecto4/src/router/Routing.jsx
+++ 17-proyecto4/src/router/Routing.jsx
@@ -7,6 +7,8 @@ import { PrivateLayout } from "../components/layout/private/PrivateLayout";
 import { Feed } from "../components/publication/Feed";
 import { AuthProvider } from "../context/AuthProvider";
 import { Logout } from "../components/user/Logout";
+import { People } from "../components/user/People";
+import { Config } from "../components/user/Config";
 
 export const Routing = () => {
     return (
@@ -25,6 +27,8 @@ export const Routing = () => {
                         <Route index element={<Feed />} />
                         <Route path="feed" element={<Feed />} />
                         <Route path="logout" element={<Logout />} />
+                        <Route path="gente" element={<People />} />
+                        <Route path="ajustes" element={<Config/>} />
                     </Route>
 
                     {/* RUTA DE ERROR 404 */}
