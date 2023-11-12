addfd1c8648c0a0d657bfcf2791790e289699e42
Carlos Villegas <carlos.villegasch@gmail.com>
2023-11-12 13:26:57 -0500

    Load user's data within Ajustes' form

--- 17-proyecto4/src/components/user/Config.jsx
+++ 17-proyecto4/src/components/user/Config.jsx
@@ -1,10 +1,17 @@
 import React, { useState } from 'react'
+import useAuth from '../../hooks/useAuth';
+import { Global } from '../../helpers/Global';
 
 export const Config = () => {
+
+  const {auth} = useAuth();
 
   const [saved, setSaved] = useState("not_saved");
 
   const updateUser = (e) => {
+
+    console.log(auth);
+
     e.preventDefault();
   }
 
@@ -31,17 +38,27 @@ export const Config = () => {
         <form className='config-form' onSubmit={updateUser}>
           <div className='form-group'>
             <label htmlFor='name'>Nombre</label>
-            <input type='text' name='name' />
+            <input type='text' name='name' defaultValue={auth.name} />
+          </div>
+
+          <div className='form-group'>
+            <label htmlFor='surname'>Apellidos</label>
+            <input type='text' name='surname' defaultValue={auth.surname} />
           </div>
 
           <div className='form-group'>
             <label htmlFor='nick'>Nickname</label>
-            <input type='text' name='nick' />
+            <input type='text' name='nick' defaultValue={auth.nick} />
+          </div>
+
+          <div className='form-group'>
+            <label htmlFor='bio'>Biografía</label>
+            <textarea name='bio' defaultValue={auth.bio} />
           </div>
 
           <div className='form-group'>
             <label htmlFor='email'>Correo Electrónico</label>
-            <input type='email' name='email' />
+            <input type='email' name='email' defaultValue={auth.email} />
           </div>
 
           <div className='form-group'>
@@ -51,9 +68,11 @@ export const Config = () => {
 
           <div className='form-group'>
             <label htmlFor='file0'>Avatar</label>
-            <div className='avatar'>
-              {/* MOSTRAR IMAGEN */}
+            <div className="general-info__container-avatar">
+                            {auth.image != "default.png" && <img src={Global.url + "user/avatar/" + auth.image} className="container-avatar__img" alt="Foto de perfil" />}
+                            {auth.image == "default.png" && <img src={avatar} className='container-avatar__img' alt="Foto de perfil" />}
             </div>
+            <br />
             <input type='file' name='file0' id="file" />
           </div>
           <br/>
