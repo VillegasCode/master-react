2e8bfd6542f8709fcbe3dd9db68333364e0a8aa9
Carlos Villegas <carlos.villegasch@gmail.com>
2023-11-21 19:56:24 -0500

    It goes to load the profiles of our friends on our Social network

--- 17-proyecto4/src/components/user/People.jsx
+++ 17-proyecto4/src/components/user/People.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import avatar from '../../assets/img/user.png';
+import { Global } from '../../helpers/Global';
 
 export const People = () => {
+
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  const getUsers = async () => {
+    //Petición para sacar usuarios
+    const request = await fetch(Global.url + 'user/list/1', {
+      method: "GET",
+      headers: {
+        "Content-type": "application/json",
+        "Authorization": localStorage.getItem("token")
+      }
+    });
+
+    const data = await request.json();
+
+    //Crear un estado para poder listar usuarios
+    if (data.users && data.status == "success") {
+      setUsers(data.users);
+      console.log(data.users);
+      //Paginacion
+
+    }
+  }
+
   return (
     <section className="layout__content">
 
@@ -11,44 +40,53 @@ export const People = () => {
 
       <div className="content__posts">
 
-        <article className="posts__post">
+        {users.map(user => {
+          return (
+            <article className="posts__post" key={user._id}>
 
-          <div className="post__container">
+              <div className="post__container">
 
-            <div className="post__image-user">
-              <a href="#" className="post__image-link">
-                <img src={avatar} className="post__user-image" alt="Foto de perfil" />
-              </a>
-            </div>
+                <div className="post__image-user">
+                  <a href="#" className="post__image-link">
+                    {user.image != "default.png" && <img src={Global.url + "user/avatar/" + user.image} className="post__user-image" alt="Foto de perfil" />}
+                    {user.image == "default.png" && <img src={avatar} className='post__user-image' alt="Foto de perfil" />}
+                  </a>
+                </div>
 
-            <div className="post__body">
+                <div className="post__body">
 
-              <div className="post__user-info">
-                <a href="#" className="user-info__name">Victor Robles</a>
-                <span className="user-info__divider"> | </span>
-                <a href="#" className="user-info__create-date">Hace 1 hora</a>
+                  <div className="post__user-info">
+                    <a href="#" className="user-info__name">{user.name} {user.surname}</a>
+                    <span className="user-info__divider"> | </span>
+                    <a href="#" className="user-info__create-date">{user.created_at}</a>
+                  </div>
+
+                  <h4 className="post__content">{user.bio}</h4>
+
+                </div>
+
               </div>
 
-              <h4 className="post__content">Hola, buenos dias.</h4>
 
-            </div>
+              <div className="post__buttons">
 
-          </div>
+                <a href="#" className="post__button post__">
+                  Seguir
+                </a>
+
+                {/* <a href="#" className="post__button">
+      Dejar de seguir
+    </a> */}
+
+              </div>
+
+            </article>
+
+          );
+        })
+        }
 
 
-          <div className="post__buttons">
-
-            <a href="#" className="post__button post__">
-              Seguir
-            </a>
-
-            {/* <a href="#" className="post__button">
-              Dejar de seguir
-            </a> */}
-
-          </div>
-
-        </article>
 
       </div>
 
