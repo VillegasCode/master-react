a94675792bcbd36cea541614e4a0ccce744ace7e
Carlos Villegas <carlos.villegasch@gmail.com>
2023-11-28 20:21:54 -0500

    We've created 2 components the first one is named Following.jsx and the second one is named Followers.jsx

---
+++ 17-proyecto4/src/components/follow/Followers.jsx
@@ -0,0 +1,73 @@
+import React, { useEffect, useState } from 'react'
+import { Global } from '../../helpers/Global';
+import { UserList } from '../user/UserList';
+
+export const Followers = () => {
+
+  const [users, setUsers] = useState([]);
+  const [page, setPage] = useState(1);
+  const [more, setMore] = useState(true);
+  const [loading, setLoading] = useState(true);
+  const [following, setFollowing] = useState([]);
+
+  useEffect(() => {
+    getUsers(1);
+  }, []);
+
+  const getUsers = async (nextPage = 1) => {
+    //Efecto de carga
+    setLoading(true);
+
+    //Petición para sacar usuarios
+    const request = await fetch(Global.url + 'follow/followers/' + nextPage, {
+      method: "GET",
+      headers: {
+        "Content-type": "application/json",
+        "Authorization": localStorage.getItem("token")
+      }
+    });
+
+    const data = await request.json();
+
+    //Crear un estado para poder listar usuarios
+    if (data.users && data.status == "success") {
+      let newUsers = data.users;
+
+      if (users.length >= 1) {
+        newUsers = [...users, ...data.users];
+      }
+
+      setUsers(newUsers);
+      setFollowing(data.user_following);
+      //Termina de hacer la petición AJAX entonces se vuelve false el estado LOADING
+      setLoading(false);
+
+      //Paginación
+      if (users.length >= (data.total - data.users.length)) {
+        setMore(false);
+      }
+    }
+  }
+
+
+
+  return (
+    <section className="layout__content">
+
+      <header className="content__header">
+        <h1 className="content__title">GENTE</h1>
+      </header>
+
+      <UserList users={users}
+        getUsers={getUsers}
+        following={following}
+        setFollowing={setFollowing}
+        page={page}
+        setPage={setPage}
+        more={more}
+        loading={loading}
+      />
+      <br />
+    </section>
+  )
+}
---
+++ 17-proyecto4/src/components/follow/Following.jsx
@@ -0,0 +1,81 @@
+import React, { useEffect, useState } from 'react'
+import { Global } from '../../helpers/Global';
+import { UserList } from '../user/UserList';
+import { useParams } from 'react-router-dom';
+
+export const Following = () => {
+
+  const [users, setUsers] = useState([]);
+  const [page, setPage] = useState(1);
+  const [more, setMore] = useState(true);
+  const [loading, setLoading] = useState(true);
+  const [following, setFollowing] = useState([]);
+
+  const params = useParams();
+  
+  useEffect(() => {
+    getUsers(1);
+  }, []);
+
+  const getUsers = async (nextPage = 1) => {
+    //Efecto de carga
+    setLoading(true);
+
+
+    //Sacar userId de la url
+    const userId = params.userId;
+
+    //Petición para sacar usuarios
+    const request = await fetch(Global.url + 'follow/following/' + userId + "/" + nextPage, {
+      method: "GET",
+      headers: {
+        "Content-type": "application/json",
+        "Authorization": localStorage.getItem("token")
+      }
+    });
+
+    const data = await request.json();
+
+    //Crear un estado para poder listar usuarios
+    if (data.follows && data.status == "success") {
+
+      let newUsers = data.follows;
+
+      if (users.length >= 1) {
+        newUsers = [...users, ...data.follows];
+      }
+
+      setUsers(newUsers);
+      setFollowing(data.user_following);
+      //Termina de hacer la petición AJAX entonces se vuelve false el estado LOADING
+      setLoading(false);
+
+      //Paginación
+      if (users.length >= (data.total - data.follows.length)) {
+        setMore(false);
+      }
+    }
+  }
+
+
+
+  return (
+    <section className="layout__content">
+
+      <header className="content__header">
+        <h1 className="content__title">Usuarios que sigue NOMBRE USUARIO</h1>
+      </header>
+
+      <UserList users={users}
+        getUsers={getUsers}
+        following={following}
+        setFollowing={setFollowing}
+        page={page}
+        setPage={setPage}
+        more={more}
+        loading={loading}
+      />
+      <br />
+    </section>
+  )
+}
