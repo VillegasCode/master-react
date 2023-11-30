7c828f203ec75a74e67f82148089e2d3b6146abe
Carlos Villegas <carlos.villegasch@gmail.com>
2023-11-30 12:50:57 -0500

    We've created the SavePublication method, this helps us for post a publication with text and image and save them into database

--- 17-proyecto4/src/components/layout/private/Sidebar.jsx
+++ 17-proyecto4/src/components/layout/private/Sidebar.jsx
@@ -3,13 +3,66 @@ import avatar from '../../../assets/img/user.png';
 import { Global } from '../../../helpers/Global';
 import useAuth from '../../../hooks/useAuth';
 import { Link } from 'react-router-dom';
-
+import { useForm } from '../../../hooks/useForm';
+import { useState } from 'react';
 
 export const Sidebar = () => {
 
-    const {auth, counters} = useAuth();
+    const { auth, counters } = useAuth();
+    const { form, changed } = useForm({});
+    const [stored, setStored] = useState("");
 
-    //console.log(auth);
+    const savePublication = async (e) => {
+        e.preventDefault();
+
+        //Recoger los datos del formulario
+        let newPublication = form;
+        newPublication.user = auth._id;
+        const token = localStorage.getItem("token");
+
+        //Hacer request para guardar la BD
+        const request = await fetch(Global.url + "publication/save", {
+            method: "POST",
+            body: JSON.stringify(newPublication),
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": token,
+            }
+        });
+
+        const data = await request.json();
+
+        //Mostrar mensajes de éxito o error
+        if (data.status = "success") {
+            setStored("stored");
+        } else {
+            setStored("error");
+        }
+
+        //Subir imagen
+        const fileInput = document.querySelector("#file");
+
+        if(data.status == "success" && fileInput.files[0]){
+            const formData = new FormData();
+            formData.append("file0", fileInput.files[0]);
+
+            const uploadRequest = await fetch(Global.url + "publication/upload/" + data.publicationStored._id, {
+                method: "POST",
+                body: formData,
+                headers: {
+                    "Authorization": token
+                }
+            });
+
+            const uploadData = await uploadRequest.json();
+
+            if(uploadData.status == "success"){
+                setStored("stored");
+            } else {
+                setStored("error");
+            }
+        }
+    }
 
     return (
         <aside className="layout__aside">
@@ -37,7 +90,7 @@ export const Sidebar = () => {
                     <div className="profile-info__stats">
 
                         <div className="stats__following">
-                            <Link to={"siguiendo/" + auth._id}  className="following__link">
+                            <Link to={"siguiendo/" + auth._id} className="following__link">
                                 <span className="following__title">Siguiendo</span>
                                 <span className="following__number">{counters.following}</span>
                             </Link>
@@ -63,20 +116,26 @@ export const Sidebar = () => {
 
 
                 <div className="aside__container-form">
+                    {stored == "stored" ?
+                        <strong className='alert alert-success'>Publicada correctamente!"</strong>
+                        : ''}
 
-                    <form className="container-form__form-post">
+
+                    {stored == "error" ?
+                        <strong className='alert alert-danger'>"Error, no se ha publicado nada"</strong> : ''}
+                    <form className="container-form__form-post" onSubmit={savePublication}>
 
                         <div className="form-post__inputs">
-                            <label htmlFor="post" className="form-post__label">¿Que estas pesando hoy?</label>
-                            <textarea name="post" className="form-post__textarea"></textarea>
+                            <label htmlFor="text" className="form-post__label">¿Que estas pesando hoy?</label>
+                            <textarea name="text" className="form-post__textarea" onChange={changed} />
                         </div>
 
                         <div className="form-post__inputs">
-                            <label htmlFor="image" className="form-post__label">Sube tu foto</label>
-                            <input type="file" name="image" className="form-post__image" />
+                            <label htmlFor="file" className="form-post__label">Sube tu foto</label>
+                            <input type="file" name="file0" id="file" className="form-post__image" />
                         </div>
 
-                        <input type="submit" value="Enviar" className="form-post__btn-submit" disabled />
+                        <input type="submit" value="Enviar" className="form-post__btn-submit" />
 
                     </form>
 
