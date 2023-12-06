07a0b79964a3663a8794c47e1e9de122193491b4
Carlos Villegas <carlos.villegasch@gmail.com>
2023-12-06 18:59:47 -0500

    We've created a route to Profile page

--- 17-proyecto4/src/router/Routing.jsx
+++ 17-proyecto4/src/router/Routing.jsx
@@ -11,6 +11,7 @@ import { People } from "../components/user/People";
 import { Config } from "../components/user/Config";
 import { Followers } from "../components/follow/Followers";
 import { Following } from "../components/follow/Following";
+import { Profile } from "../components/user/Profile";
 
 export const Routing = () => {
     return (
@@ -33,6 +34,7 @@ export const Routing = () => {
                         <Route path="ajustes" element={<Config />} />
                         <Route path="siguiendo/:userId" element={<Following />} />
                         <Route path="seguidores/:userId" element={<Followers />} />
+                        <Route path="perfil/:userId" element={<Profile/>} />
                     </Route>
 
                     {/* RUTA DE ERROR 404 */}
