f9b890859ad1310bd371340d5a2bb0bbccc725b4
Carlos Villegas <carlos.villegasch@gmail.com>
2024-06-07 18:42:20 -0500

    Form with FORMIK and REACT

---
+++ 18-mas-forms/src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { MiFormulario } from './components/MiFormulario.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  
+    <MiFormulario/>
+  
+)
