da31fda35c8bf1c417ecd6089cd2bcace2d53704
Carlos Villegas <carlos.villegasch@gmail.com>
2024-06-12 20:21:50 -0500

    It shows us our own posts

--- 17-proyecto4/src/components/publication/PublicationList.jsx
+++ 17-proyecto4/src/components/publication/PublicationList.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { Global } from '../../helpers/Global';
 import useAuth from '../../hooks/useAuth';
-import avatar from '../../assets/img/user.png'
+import avatar from '../../assets/img/user.png';
+import ReactTimeAgo from "react-time-ago";
 
-export const PublicationList = ({publications,
+export const PublicationList = ({
+    publications = {}, // Valor predeterminado como objeto vacío
     getPublications,
     page,
     setPage,
     more,
-    setMore}) => {
-
+    setMore
+}) => {
     const { auth } = useAuth();
 
     const nextPage = () => {
@@ -20,75 +22,76 @@ export const PublicationList = ({publications,
     }
 
     const deletePublication = async (publicationId) => {
-        const request = await fetch(Global.url + "publication/remove/" + publicationId, {
-            method: "DELETE",
-            headers: {
-                "Content-Type": "application/json",
-                "Authorization": localStorage.getItem("token")
-            }
-        });
+        try {
+            const request = await fetch(Global.url + "publication/remove/" + publicationId, {
+                method: "DELETE",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": localStorage.getItem("token")
+                }
+            });
 
-        const data = await request.json();
-        setPage(1);
-        setMore(true);
-        getPublications(1, true);
+            if (!request.ok) {
+                throw new Error("Error deleting publication");
+            }
+
+            const data = await request.json();
+            setPage(1);
+            setMore(true);
+            getPublications(1, true);
+        } catch (error) {
+            console.error("Error:", error);
+        }
     }
+
+    console.log('publications:', publications); // Verifica el valor de publications en la consola
 
     return (
         <>
             <div className="content__posts">
-
-                {publications.map(publication => {
+                {Array.isArray(publications.docs) && publications.docs.map(publication => {
                     return (
-
                         <article className="posts__post" key={publication._id}>
-
                             <div className="post__container">
-
                                 <div className="post__image-user">
                                     <Link to={"/social/perfil/" + publication.user._id} className="post__image-link">
-                                        {publication.user.image != "default.png" && <img src={Global.url + "user/avatar/" + publication.user.image} className="post__user-image" alt="Foto de perfil" />}
-                                        {publication.user.image == "default.png" && <img src={avatar} className='post__user-image' alt="Foto de perfil" />}
+                                        {publication.user.image !== "default.png" ? (
+                                            <img src={Global.url + "user/avatar/" + publication.user.image} className="post__user-image" alt="Foto de perfil" />
+                                        ) : (
+                                            <img src={avatar} className='post__user-image' alt="Foto de perfil" />
+                                        )}
                                     </Link>
                                 </div>
-
                                 <div className="post__body">
-
                                     <div className="post__user-info">
                                         <a href="#" className="user-info__name">{publication.user.name + " "}</a>
                                         <span className="user-info__divider"> | </span>
-                                        <a href="#" className="user-info__create-date">{publication.created_at}</a>
+                                        <a href="#" className="user-info__create-date">
+                                            <ReactTimeAgo date={publication.created_at} locale="es-ES" />
+                                        </a>
                                     </div>
-
                                     <h4 className="post__content">{publication.text}</h4>
-
                                     {publication.file && <img src={Global.url + "publication/media/" + publication.file} />}
-
                                 </div>
-
                             </div>
-
-
-                            {auth._id == publication.user._id &&
+                            {auth._id === publication.user._id && (
                                 <div className="post__buttons">
-
                                     <button onClick={() => deletePublication(publication._id)} className="post__button">
                                         <i className="fa-solid fa-trash-can"></i>
                                     </button>
-
                                 </div>
-                            }
-                        </article>);
+                            )}
+                        </article>
+                    );
                 })}
             </div>
-
-            {more &&
+            {more && (
                 <div className="content__container-btn">
                     <button className="content__btn-more-post" onClick={nextPage}>
-                        Ver mas publicaciones
+                        Ver más publicaciones
                     </button>
                 </div>
-            }
+            )}
         </>
-    )
+    );
 }
