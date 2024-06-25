63d6c5ddad043bc9dd6445dee39e354550cfe6e7
Carlos Villegas <carlos.villegasch@gmail.com>
2024-06-25 11:21:12 -0500

    fixed some problems

--- 17-proyecto4/src/components/publication/Feed.jsx
+++ 17-proyecto4/src/components/publication/Feed.jsx
@@ -1,26 +1,21 @@
 import React, { useEffect, useState } from 'react';
-import avatar from '../../assets/img/user.png';
-import { useParams, Link } from 'react-router-dom';
 import { Global } from '../../helpers/Global';
 import useAuth from '../../hooks/useAuth';
 import { PublicationList } from '../publication/PublicationList';
 
 export const Feed = () => {
-
     const { auth } = useAuth();
-    const [publications, setPublications] = useState([]);
+    const [publications, setPublications] = useState({ docs: [] });
     const [page, setPage] = useState(1);
     const [more, setMore] = useState(true);
-    const params = useParams();
 
     useEffect(() => {
         getPublications(1, false);
     }, []);
 
     const getPublications = async (nextPage = 1, showNews = false) => {
-
-        if(showNews){
-            setPublications([]);
+        if (showNews) {
+            setPublications({ docs: [] });
             setPage(1);
             nextPage = 1;
         }
@@ -35,45 +30,42 @@ export const Feed = () => {
 
         const data = await request.json();
 
-        if (data.status == "success") {
-            let newPublications = data.publications;
+        if (data.status === "success") {
+            let newPublications = data.publications.docs;
 
-            if (!showNews && publications.length >= 1) {
-                newPublications = [...publications, ...data.publications];
+            if (!showNews && publications.docs.length >= 1) {
+                newPublications = [...publications.docs, ...data.publications.docs];
             }
 
-            setPublications(newPublications);
+            setPublications({ ...publications, docs: newPublications });
 
-            if (!showNews && publications.length >= (data.total - data.publications.length)) {
+            if (!showNews && publications.docs.length >= (data.totalDocs - data.publications.docs.length)) {
                 setMore(false);
             }
 
-            if(data.pages <= 1){
+            if (data.pages <= 1) {
                 setMore(false);
             }
-
         }
     }
 
     return (
         <section className="layout__content">
-
             <header className="content__header">
                 <h1 className="content__title">Timeline</h1>
                 <button className="content__button" onClick={() => getPublications(1, true)}>Mostrar nuevas</button>
             </header>
 
             <PublicationList 
-                publications = {publications}
-                getPublications = {getPublications}
-                page = {page}
-                setPage = {setPage}
-                more = {more}
-                setMore = {setMore}
+                publications={publications}
+                getPublications={getPublications}
+                page={page}
+                setPage={setPage}
+                more={more}
+                setMore={setMore}
             />
             
             <br />
-
         </section>
-    )
+    );
 }
