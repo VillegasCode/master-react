477144bc5969ac002ee5b21e3656045a5eff0b61
Carlos Villegas <carlos.villegasch@gmail.com>
2024-06-25 11:16:28 -0500

    We've create a const named user.created_at to get a correct format

--- 17-proyecto4/src/components/follow/Following.jsx
+++ 17-proyecto4/src/components/follow/Following.jsx
@@ -1,107 +1,68 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { Global } from '../../helpers/Global';
 import { UserList } from '../user/UserList';
 import { useParams } from 'react-router-dom';
 import { GetProfile } from '../../helpers/GetProfile';
 
 export const Following = () => {
-
   const [users, setUsers] = useState([]);
   const [page, setPage] = useState(1);
   const [more, setMore] = useState(true);
   const [loading, setLoading] = useState(true);
   const [following, setFollowing] = useState([]);
   const [userProfile, setUserProfile] = useState({});
-
   const params = useParams();
-  
+
   useEffect(() => {
     getUsers(1);
-    GetProfile(params.userId, setUserProfile); //VER VIDEO 324
-  }, []);
+    GetProfile(params.userId, setUserProfile);
+  }, [params.userId]);
 
   const getUsers = async (nextPage = 1) => {
-    //Efecto de carga
     setLoading(true);
-
-
-    //Sacar userId de la url
     const userId = params.userId;
 
-    //Petición para sacar usuarios
-    const request = await fetch(Global.url + 'follow/following/' + userId + "/" + nextPage, {
-      method: "GET",
-      headers: {
-        "Content-type": "application/json",
-        "Authorization": localStorage.getItem("token")
-      }
-    });
+    try {
+      const request = await fetch(`${Global.url}follow/following/${userId}/${nextPage}`, {
+        method: "GET",
+        headers: {
+          "Content-type": "application/json",
+          "Authorization": localStorage.getItem("token")
+        }
+      });
 
-    const data = await request.json();
-    console.log(data);
-    //Recorrer y limpiar follows para quedarme con followed
-    let cleanUsers = [];
-    data.follows.forEach(follow => {
-        cleanUsers = [...cleanUsers, follow.followed]
-    });
+      const data = await request.json();
+      console.log(data); // Verificar la respuesta en la consola
 
-    data.users = cleanUsers;
-    console.log("DATA USERS: " + data.users);
+      if (data.status === "success") {
+        let cleanUsers = data.follows.map(follow => follow.followed);
 
-    
+        setUsers(prevUsers => nextPage === 1 ? cleanUsers : [...prevUsers, ...cleanUsers]);
+        setFollowing(data.user_following);
+        setLoading(false);
 
-    //Crear un estado para poder listar usuarios
-    if (data.users && data.status == "success") {
-
-      let newUsers = data.users;
-
-      if (users.length >= 1) {
-        newUsers = [...users, ...data.users];
-      }
-
-      console.log(newUsers);
-
-      setUsers(newUsers);
-      setFollowing(data.user_following);
-      //Termina de hacer la petición AJAX entonces se vuelve false el estado LOADING
-      setLoading(false);
-
-      //Paginación
-      if (users.length >= (data.total - data.users.length)) {
+        if (nextPage >= data.totalPages) {
+          setMore(false);
+        }
+      } else {
+        setLoading(false);
         setMore(false);
       }
+    } catch (error) {
+      console.error("Error fetching users:", error);
+      setLoading(false);
+      setMore(false);
     }
   }
 
-  //(Actualmente está en un helper llamado GetProfile) Método para sacar datos del usuario logueado por su id
-  //Este método lo sacamos cada vez que recargamos la página POR ESO LO PONEMOS EN EL useEffect
-  //   const getProfile = async() => {
-  //   const request = await fetch(Global.url + "user/profile/" + params.userId, {
-  //     method: "GET",
-  //     headers: {
-  //       "Content-Type" : "application/json",
-  //       "Authorization": localStorage.getItem("token")
-  //     }
-  //   });
-
-  //   //Obtengo los datos en formato JSON de la petición ajax
-  //   const data = await request.json();
-
-  //   if (data.status == "success") {
-  //     setUserProfile(data.Profile);
-  //   }
-  //   //console.log(data.Profile.name);
-  // }
-
-
   return (
     <section className="layout__content">
-
       <header className="content__header">
         <h1 className="content__title">{userProfile.name} {userProfile.surname} sigue a:</h1>
       </header>
 
-      <UserList users={users}
+      <UserList 
+        users={users}
         getUsers={getUsers}
         following={following}
         setFollowing={setFollowing}
@@ -112,5 +73,5 @@ export const Following = () => {
       />
       <br />
     </section>
-  )
+  );
 }
