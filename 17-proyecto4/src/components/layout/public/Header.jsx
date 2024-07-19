666a36964f6badf3ffe87530d76e508655352d00
Carlos Villegas <carlos.villegasch@gmail.com>
2024-07-18 19:47:22 -0500

    Public Header.jsx Finished successfully 18-07-2024

--- 17-proyecto4/src/components/layout/public/Header.jsx
+++ 17-proyecto4/src/components/layout/public/Header.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Nav } from './Nav'
+import { NavLink } from 'react-router-dom'
 
 export const Header = () => {
     return (
         <header className="layout__navbar">
 
             <div className="navbar__header">
-                <a href="#" className="navbar__title">REACT</a>
+                <NavLink to="/login" className="navbar__title">hit10Xharder</NavLink>
             </div>
             <Nav />
         </header>
